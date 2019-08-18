bdcb8a1d75ddc0903a79c8399b5488f8289216d5
Dyldinski <dubois.a.dylan@gmail.com>
2019-08-18 16:19:34 -0500

    Init

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
+
+const $headerSection = $(".header");
+const $aboutSection = $("#about");
+const $educationSection = $("#education");
+const $navList = $(".nav-list");
+
+$headerSection.waypoint(
+  function(direction) {
+    if (direction === "down") {
+      $navList.addClass("show-nav");
+    } else {
+      $navList.removeClass("show-nav");
+    }
+  },
+  {
+    offset: "-70%"
+  }
+);
+
+$aboutSection.waypoint(function(direction) {
+  $aboutSection.addClass("show");
+  if (screen.width > 500) {
+    updateActiveLink(1, true);
+    updateActiveLink(2, false);
+  }
+});
+
+$educationSection.waypoint(function(direction) {
+  $educationSection.addClass("show");
+  if (screen.width > 500) {
+    updateActiveLink(1, false);
+    updateActiveLink(2, true);
+  }
+});
+
+updateActiveLink = (index, isAdded) => {
+  if (isAdded) $(`.nav-list ul li:nth-child(${index}) a`).addClass("active");
+  else $(`.nav-list ul li:nth-child(${index}) a`).removeClass("active");
+};
